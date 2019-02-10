c341bc4998b52afbc3762799ecd829c4b1c045b4
UraniumBlood <vivekgopalkrishnan@gmail.com>
2019-02-10 18:02:00 +0530

    -

---
+++ WEEK_08/Tuple.py
@@ -0,0 +1,34 @@
+# # # if 19 in (1,2,3):
+# # #     print("yes")
+# # #
+# # # else:
+# # #     print("no")
+# #
+# # a = {1 : "number one", 22 : "number twenty two"}
+# # print(a[22])
+#
+# dict_employee = {'Employee name': 'Vivek', 'Age': 13, 'Designation': 'Astronomer'}
+#
+# # print(dict_employee)
+# #
+# # dict_employee['Employee name'] = "Vivek GK"
+# # dict_employee['Designation'] = "Senior Astronomer"
+# # print(dict_employee)
+# # # dict_employee.clear()
+# # # print(dict_employee)
+# # print(dict_employee.values())
+# # print(len(dict_employee))
+# #
+# # dict_employee.update({"hometown":'Kochi'})
+# # print(dict_employee)
+# # dict_employee.update({"Age": '23'})
+# # print(dict_employee)
+#
+# import json
+# x = '{"name":"John", "age":30, "city":"New York"}'
+# print(type(x))
+#
+# y = json.loads(x)
+# print(type(y))
+# print(y["age"])
+
--- Week_07/h.py
+++ Week_07/h.py
@@ -1,15 +1,15 @@
-# str = input("Enter a sentence")
-# print(str)
-# print(str.lower())
-# print(str.upper())
-# print(str.capitalize())
-
-num = [6, 7, 10, -10, 15]
-l = (len(num))
-sum = 0
-count = 0
-while(count<l):
-    sum = sum + num[count]
-    count = count + 1
-
-print(sum)
+# # str = input("Enter a sentence")
+# # print(str)
+# # print(str.lower())
+# # print(str.upper())
+# # print(str.capitalize())
+#
+# num = [6, 7, 10, -10, 15]
+# l = (len(num))
+# sum = 0
+# count = 0
+# while(count<l):
+#     sum = sum + num[count]
+#     count = count + 1
+#
+# print(sum)
